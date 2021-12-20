de2410d9b8931521f148e553022d142a0d13854b
OniriCorpe <oniricorpe@oniricorpe.eu>
2021-12-20 23:01:43 +0100

    Avancée du code & readme

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# motion
+# Motion
 
-A shitty 🦝 project to display the next events of my Notion calendar on an small e-ink screen ([Inky Phat](https://shop.pimoroni.com/products/inky-phat?variant=12549254217811)) connected to a Raspberry Pi Zero W.
+A shitty 🦝 project to display the next events of my Notion calendar on an small e-ink screen ([Pimoroni's Inky Phat](https://shop.pimoroni.com/products/inky-phat)) connected to a Raspberry Pi Zero W.
+
+
+## Usage
+
+Copy the configuration example file and rename it to 'config.py'.
+Configure your [Notion Token](https://developers.notion.com/docs/getting-started) and your [database ID](https://developers.notion.com/docs/getting-started#step-2-share-a-database-with-your-integration).
+Please note that this program is designed for [this agenda template](https://xaanaa.notion.site/Agenda-template-bc1b0b00787e44a18218cde30f71f524). You can freely adapt the code to your usage.
+
+Optionally, you can activate the display of the current date and the display of an indicator that means that at least one of the elements of [this medical inventory template](https://xaanaa.notion.site/Inventaire-m-dical-template-4b72c6408bbd41878661705e2d9d5b37) is to be re-stocked.
---
+++ main.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import os
+import notion_client
+from notion_client import Client
+from datetime import date, time
+import json
+import config
+
+# debug
+from pprint import pprint
+
+
+# init the notion token
+if config.NOTION_TOKEN != "": # check that the notion token id is filled in
+    notion = Client(auth=config.NOTION_TOKEN)
+else: # if the notion token isn't filled in
+    print("Please configure your Notion Token")
+
+
+# get and print today's date without year
+if config.show_date: # if "show_date" is true in the config file, show the current date
+    today = date.today()
+    print (today.strftime("%d/%m"))
+
+
+if config.agenda_db_id != "": # check that the db id is filled in
+    next_events = notion.databases.query(
+        **{
+            "database_id": config.agenda_db_id,
+            "filter": {
+                "property": "Date",
+                "date": {
+                    "next_week": {},
+                },
+            },
+        }
+    )
+    pprint(next_events)
+else: # if the db id isn't filled in
+    print("Please configure your database ID 'agenda_db_id' in the config file")
+
+
+if config.meds_db_id != "": # check that the db id is filled in
+    meds_refill = notion.databases.query(
+        **{
+            "database_id": config.meds_db_id,
+            "filter": {
+                "property": "Refill",
+                "checkbox": {
+                    "equals": True,
+                },
+            },
+        }
+    )
+    pprint(meds_refill)
