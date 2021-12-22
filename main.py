8a8714030399e1526b2065c338e22110e0591b1c
OniriCorpe <oniricorpe@disroot.org>
2021-12-22 01:05:18 +0100

    Ajout du support des dates de fin pour les événements

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ if config.show_date:  # if 'show_date' is true in the configuration file, displa
 
 
 if config.agenda_db_id != "":  # check that the database ID is filled
-    inaweek = today + timedelta(days = 7)  # edit the number of days if you want
+    # edit the number of days if you want
+    inaweek = today + timedelta(days = 7)
     next_events = notion.databases.query(  # query to the notion API
         **{
             "database_id": config.agenda_db_id,  # select the database to query
@@ -36,13 +37,15 @@ if config.agenda_db_id != "":  # check that the database ID is filled
                     {
                         "property": "Date",  # edit with the name of the property to filter
                         "date": {  # we want the current and future items
-                            "on_or_after": today.strftime("%Y-%m-%d"),  # Date filter condition: https://developers.notion.com/reference/post-database-query#date-filter-condition
+                            # Date filter condition: https://developers.notion.com/reference/post-database-query#date-filter-condition
+                            "on_or_after": today.strftime("%Y-%m-%d"),
                         },
                     },
                     {
                         "property": "Date",  # edit with the name of the property to filter
-                        "date":   {# we want the items in a week and before
-                            "on_or_before": inaweek.strftime("%Y-%m-%d"),  # Date filter condition: https://developers.notion.com/reference/post-database-query#date-filter-condition
+                        "date":   {  # we want the items in a week and before
+                            # Date filter condition: https://developers.notion.com/reference/post-database-query#date-filter-condition
+                            "on_or_before": inaweek.strftime("%Y-%m-%d"),
                         },
                     },
                 ]
@@ -58,17 +61,35 @@ if config.agenda_db_id != "":  # check that the database ID is filled
     # print all items that come in this rolling week
     for item in next_events['results']:
         # edit 'Date' with the name of the property of your database
-        date = item['properties']['Date']['date']['start']
-        if "T" in date:  # check if 'date' has hour data
-            date_day = date[8:len(date)-19]  # get day
-            date_month = date[5:len(date)-22]  # get month
-            date_hour = date[11:len(date)-13]  # get hour
-            date = date_day + "/" + date_month + " " + date_hour  # combine day, month and hour
-        elif "T" not in date:  # check if 'date' has no hour data
-            date_day = date[8:]  # get day
-            date_month = date[5:7]  # get month
-            date = date_day + "/" + date_month  # combine day and month
-        nb = item['properties']['Nb Jours']['formula']['string']  # edit 'Nb Jours' with the name in your database
+        # get item starting
+        date_start = item['properties']['Date']['date']['start']
+        if "T" in date_start:  # check if 'date' has hour data
+            date_day = date_start[8:len(date)-19]  # get day
+            date_month = date_start[5:len(date)-22]  # get month
+            date_hour = date_start[11:len(date)-13]  # get hour
+            date_start = date_day + "/" + date_month + " " + \
+                date_hour  # combine day, month and hour
+        elif "T" not in date_start:  # check if 'date' has no hour data
+            date_day = date_start[8:]  # get day
+            date_month = date_start[5:7]  # get month
+            date_start = date_day + "/" + date_month  # combine day and month
+        # edit 'Date' with the name of the property of your database
+        date_end = item['properties']['Date']['date']['end']  # get item ending
+        if date_end is not None:  # if there is an end date
+            if "T" in date_end:  # check if 'date' has hour data
+                date_day = date_end[8:len(date)-19]  # get day
+                date_month = date_end[5:len(date)-22]  # get month
+                date_hour = date_end[11:len(date)-13]  # get hour
+                date_end = date_day + "/" + date_month + " " + \
+                    date_hour  # combine day, month and hour
+            elif "T" not in date_end:  # check if 'date' has no hour data
+                date_day = date_end[8:]  # get day
+                date_month = date_end[5:7]  # get month
+                date_end = date_day + "/" + date_month  # combine day and month
+        else:  # if there is not an end date
+            date_end = False
+        # edit 'Nb Jours' with the name in your database
+        nb = item['properties']['Nb Jours']['formula']['string']
         if "Dans" in nb:  # if 'Dans' is found in the 'nb' variable
             nb = nb[5:len(nb)-6] + " j"
         elif "Aujourd’hui" in nb:  # if 'Aujourd’hui' is found in the 'nb' variable
@@ -77,7 +98,10 @@ if config.agenda_db_id != "":  # check that the database ID is filled
             nb = "dem"
         # edit 'Nom' with the name of your database
         name = item['properties']['Nom']['title'][0]['plain_text']
-        print(date + " " + nb + " " + name)
+        if date_end is False:  # if there is not an end date
+            print(date_start + " " + nb + " " + name)
+        else:  # if there is an end date
+            print(date_start[:2] + "-" + date_end + " " + nb + " " + name)
 else:  # if the database ID is not filled
     print("Please configure your database ID 'agenda_db_id' in your config file")
 
