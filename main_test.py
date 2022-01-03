c7aeba7a46b0773857d4e8da42718c9e47a9d71a
OniriCorpe <oniricorpe@disroot.org>
2022-01-03 20:04:03 +0100

    Correction du nom du fichier de configuration de test pour passer le .gitignore

--- main_test.py
+++ main_test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import main
-from tests import config
+from tests import config_test
 
 def test_config_has_setting():
-    assert main.config_has_setting(config, "AGENDA_DB_ID") == False
-    assert main.config_has_setting(config, "NOTION_TOKEN") == True
+    assert main.config_has_setting(config_test, "AGENDA_DB_ID") == False
+    assert main.config_has_setting(config_test, "NOTION_TOKEN") == True
---
+++ tests/config_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""
+This is config file for testing purpose.
+Please do not edit.
+"""
+
+NOTION_TOKEN = "secret_xxx"
+
+AGENDA_DB_ID = ""
