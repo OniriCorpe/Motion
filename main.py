848d32f425fbcfdd5eeafd0482d7cae193b1e3f8
OniriCorpe <oniricorpe@disroot.org>
2022-01-04 03:35:55 +0100

    Réusinage du fichier de configuration

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from datetime import date, timedelta
 from pprint import pprint  # debug
 from notion_client import Client
 import peewee
-import config
+import config as cfg
 
 
 #  about WAL: https://sqlite.org/pragma.html#pragma_journal_mode
@@ -65,21 +65,7 @@ def current_date_timedelta():
     in the configuration file (NUMBER_OF_DAYS) is added.
     """
 
-    return get_current_date() + timedelta(days=config.AGENDA_NUMBER_OF_DAYS)
-
-
-def config_has_setting(config_file, config_item):
-    """
-    Arguments:
-    'config_file': The configuration file to check. Default: 'config'.
-    'config_item': The name of the constant to check in the configuration file.
-    eg: "NOTION_TOKEN" or "AGENDA_DB_ID".
-
-    The fonction returns a boolean:
-    'True' if the constant is filled.
-    'False' if the constant is empty.
-    """
-    return getattr(config_file, config_item) != ""
+    return get_current_date() + timedelta(days=cfg.AGENDA["NUMBER_OF_DAYS"])
 
 
 def init_notion():
@@ -89,12 +75,12 @@ def init_notion():
     """
 
     # check that the Notion token is filled
-    if config_has_setting(config, "NOTION_TOKEN"):
+    if "secret_" in cfg.NOTION_TOKEN:
         # if there is string, trying to init the Notion token with it
-        return Client(auth=config.NOTION_TOKEN)
+        return Client(auth=cfg.NOTION_TOKEN)
     # if the Notion token isn't filled, print error and stop the program
     sys.exit(
-        "Please configure your Notion token.\n"
+        "Please configure your Notion token in your configuration file.\n"
         "Get your token here: https://developers.notion.com/docs/getting-started"
     )
 
@@ -106,7 +92,7 @@ def show_current_date():
 
     # get and print today's date (without year)
     # if 'show_date' is true in the configuration file, display the current date
-    if config.SHOW_DATE:
+    if cfg.SHOW_DATE:
         print(get_current_date().strftime("%d/%m"))
 
 
@@ -114,14 +100,15 @@ show_current_date()
 
 
 # check that the database ID is filled
-if config_has_setting(config, "AGENDA_DB_ID"):
+# if config_has_setting(cfg, AGENDA["DB_ID"]):
+if cfg.AGENDA["DB_ID"] != "":
     next_events = init_notion().databases.query(  # query to the notion API
         **{
-            "database_id": config.AGENDA_DB_ID,  # select the database to query
+            "database_id": cfg.AGENDA["DB_ID"],  # select the database to query
             "filter": {  # get only the items that come in this rolling week
                 "and": [  # we want to satisfy the 2 conditions
                     {
-                        "property": config.AGENDA_DATE,
+                        "property": cfg.AGENDA["DATE"],
                         "date": {  # we want the current and future items
                             # Date filter condition:
                             # https://developers.notion.com/reference/post-database-query#date-filter-condition
@@ -129,7 +116,7 @@ if config_has_setting(config, "AGENDA_DB_ID"):
                         },
                     },
                     {
-                        "property": config.AGENDA_DATE,
+                        "property": cfg.AGENDA["DATE"],
                         "date": {  # we want the items in a week and before
                             # Date filter condition:
                             # https://developers.notion.com/reference/post-database-query#date-filter-condition
@@ -142,7 +129,7 @@ if config_has_setting(config, "AGENDA_DB_ID"):
             },
             "sorts": [  # sort items from nearest to farthest
                 {
-                    "property": config.AGENDA_DATE,
+                    "property": cfg.AGENDA["DATE"],
                     "direction": "ascending",
                 },
             ],
@@ -151,7 +138,7 @@ if config_has_setting(config, "AGENDA_DB_ID"):
     # print all items that come in this rolling week
     for item in next_events["results"]:
         # get item starting date
-        date_start = item["properties"][config.AGENDA_DATE]["date"]["start"]
+        date_start = item["properties"][cfg.AGENDA["DATE"]]["date"]["start"]
         if "T" in date_start:  # check if 'date' has hour data
             date_day = date_start[8:-19]  # get day
             date_month = date_start[5:-22]  # get month
@@ -165,7 +152,7 @@ if config_has_setting(config, "AGENDA_DB_ID"):
             date_month = date_start[5:7]  # get month
             date_start = f"{date_day}/{date_month}"  # combine day and month
         # get item ending date
-        date_end = item["properties"][config.AGENDA_DATE]["date"]["end"]
+        date_end = item["properties"][cfg.AGENDA["DATE"]]["date"]["end"]
         if date_end is not None:  # if there is an end date
             if "T" in date_end:  # check if 'date' has hour data
                 date_day = date_end[8:-19]  # get day
@@ -179,20 +166,19 @@ if config_has_setting(config, "AGENDA_DB_ID"):
                 date_day = date_end[8:]  # get day
                 date_month = date_end[5:7]  # get month
                 date_end = f"{date_day}/{date_month}"  # combine day and month
-        number_of_days_before = item["properties"][config.AGENDA_REMAINING_DAYS][
+        number_of_days_before = item["properties"][cfg.AGENDA["REMAINING_DAYS"]][
             "formula"
         ]["string"]
         # if 'Dans' is found in the 'number_of_days_before' variable
-        if config.AGENDA_FILTER_IN_DAYS in number_of_days_before:
-            number_of_days_before = (
-                f"{number_of_days_before[5:-6]}{config.AGENDA_IN_DAYS}"
-            )
+        if cfg.AGENDA["FILTER_IN_DAYS"] in number_of_days_before:
+            IN_DAYS = cfg.AGENDA["IN_DAYS"]
+            number_of_days_before = f"{number_of_days_before[5:-6]}{IN_DAYS}"
         # if 'Aujourd’hui' is found in the 'number_of_days_before' variable
-        elif config.AGENDA_FILTER_TODAY in number_of_days_before:
-            number_of_days_before = config.AGENDA_TODAY
-        elif config.AGENDA_FILTER_TOMORROW in number_of_days_before:
-            number_of_days_before = config.AGENDA_TOMORROW
-        name = item["properties"][config.AGENDA_NAME]["title"][0]["plain_text"]
+        elif cfg.AGENDA["FILTER_TODAY"] in number_of_days_before:
+            number_of_days_before = cfg.AGENDA["TODAY"]
+        elif cfg.AGENDA["FILTER_TOMORROW"] in number_of_days_before:
+            number_of_days_before = cfg.AGENDA["TOMORROW"]
+        name = item["properties"][cfg.AGENDA["NAME"]]["title"][0]["plain_text"]
         if date_end is None:  # if there is not an end date
             print(f"{date_start} {number_of_days_before} {name}")
         else:  # if there is an end date
@@ -201,12 +187,12 @@ else:  # if the database ID is not filled
     print("Please configure your database ID 'AGENDA_DB_ID' in your config file")
 
 # check that the database ID is filled
-if config_has_setting(config, "MEDS_DB_ID"):
+if cfg.MEDS["DB_ID"] != "":
     meds = init_notion().databases.query(  # query to the notion API
         **{
-            "database_id": config.MEDS_DB_ID,  # select the database to query
+            "database_id": cfg.MEDS["DB_ID"],  # select the database to query
             "filter": {  # get only the elements that need to be restocked
-                "property": config.MEDS_REFILL,
+                "property": cfg.MEDS["REFILL"],
                 "checkbox": {
                     "equals": True,
                 },
@@ -217,9 +203,9 @@ if config_has_setting(config, "MEDS_DB_ID"):
     if meds["results"]:
         for item in meds["results"]:
             # get item name
-            name_meds = item["properties"][config.MEDS_NAME]["title"][0]["plain_text"]
+            name_meds = item["properties"][cfg.MEDS["NAME"]]["title"][0]["plain_text"]
             # get the minimum number of units to be restocked
-            nb_refill = item["properties"][config.MEDS_NUMBER]["formula"]["number"]
+            nb_refill = item["properties"][cfg.MEDS["NUMBER"]]["formula"]["number"]
             pprint(f"{name_meds} : ≥{nb_refill}")
     else:  # there is no item to restock
         print("rien à restock")
