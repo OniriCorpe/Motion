92025cf724fc15ca067eed6e734bfe7a7d87e6b3
OniriCorpe <oniricorpe@disroot.org>
2022-01-06 21:46:31 +0100

    Réusinage de la fonction de tests du token notion et ajout d'un test pytest idoine

--- main_test.py
+++ main_test.py
@@ -6,3 +6,20 @@ The test file to use with pytest.
 """
 
 import main
+
+
+def test_check_notion_token():
+    """
+    [summary]
+    """
+
+    assert (
+        main.check_notion_token("secret_xxpZwWkuRip7eXipUC7D2fS2sCMBi3v9KFq7aT46HFQ")
+        is True
+    )
+    assert (
+        main.check_notion_token("ojmvCx5XjJUfZziJ9a3AHQKGHcLdy2zKNVfNQa5YtFZaAQSbPZ")
+        is False
+    )
+    assert main.check_notion_token("secret_test") is False
+    assert main.check_notion_token("") is False
