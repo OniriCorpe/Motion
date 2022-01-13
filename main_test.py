7aa88041a60c4cfd05a24a0b99d2daec18fa85b0
OniriCorpe <oniricorpe@disroot.org>
2022-01-13 03:39:08 +0100

    Ajouts trys & excepts / nettoyage du code

--- main_test.py
+++ main_test.py
@@ -8,55 +8,6 @@ The test file to use with pytest.
 import main
 
 
-def test_check_notion_token():
-    """
-    Tests the check_notion_token(token) function with valid and invalids tokens.
-
-    A valid Notion token contains "secret_" at the beginning and is 50 characters long.
-    """
-
-    assert (  # 50 chars with "secret_" at the beginning
-        main.check_notion_token("secret_xxpZwWkuRip7eXipUC7D2fS2sCMBi3v9KFq7aT46HFQ")
-        is True
-    )
-    assert (  # 50 chars with "secret_" in the middle
-        main.check_notion_token("xxpZwWkuRip7eXipUsecret_C7D2fS2sCMBi3v9KFq7aT46HFQ")
-        is False
-    )
-    assert (  # 50 chars without "secret_"
-        main.check_notion_token("ojmvCx5XjJUfZziJ9a3AHQKGHcLdy2zKNVfNQa5YtFZaAQSbPZ")
-        is False
-    )
-    assert main.check_notion_token("secret_test") is False  # 11 chars with secret
-    assert main.check_notion_token("") is False  # nothing
-
-
-def test_check_db_id():
-    """
-    Tests the check_db_id(db_id) function with valid and invalids IDs.
-
-    A valid ID is 32 characters long.
-    """
-
-    assert main.check_db_id("Uz79xghKTpgsnHNXdLJpbh98ey6rL6kx") is True  # 32
-    assert main.check_db_id("Uz79xghKTpgsnHNXdLJpbh98ey6rL6kxx") is False  # 34
-    assert main.check_db_id("Uz79xghKTpgsnHNXdLJpbh98ey6rL6") is False  # 30
-    assert main.check_db_id("") is False  # 0
-
-
-def test_agenda_format_date():
-    """
-    Tests that the function agenda_format_date(data) formats correctly under all conditions.
-    """
-
-    assert main.agenda_format_date("2022-12-13") == "13/12"
-    assert main.agenda_format_date("2024-01-27T14:30:00") == "27/01 14:30"
-    assert main.agenda_format_date("2022-01-09T15:00:00.000") == "09/01 15:00"
-    assert main.agenda_format_date("2021-10-15T12:00:00-07:00") == "15/10 12:00"
-    assert main.agenda_format_date("2022-01-09T15:00:00.000+01:00") == "09/01 15:00"
-    assert main.agenda_format_date(None) is None
-
-
 def test_agenda_results():
     """
     Tests that the function agenda_results(data) formats correctly under all conditions.
@@ -69,12 +20,6 @@ def test_agenda_results():
                 "results": [
                     {
                         "properties": {
-                            "Date": {
-                                "date": {
-                                    "start": "2022-12-13",
-                                    "end": None,
-                                },
-                            },
                             "Nb Jours": {
                                 "formula": {
                                     "string": "🚨 Aujourd’hui",
@@ -92,19 +37,13 @@ def test_agenda_results():
                 ],
             }
         )
-        == [("13/12", "ajd", "test 1")]
+        == [("ajd", "test 1")]
     )
     assert main.agenda_results(
         {
             "results": [
                 {
                     "properties": {
-                        "Date": {
-                            "date": {
-                                "start": "2022-12-08",
-                                "end": None,
-                            },
-                        },
                         "Nb Jours": {
                             "formula": {
                                 "string": "🚨 Aujourd’hui",
@@ -121,12 +60,6 @@ def test_agenda_results():
                 },
                 {
                     "properties": {
-                        "Date": {
-                            "date": {
-                                "start": "2022-12-09T13:12:00.000+01:00",
-                                "end": None,
-                            },
-                        },
                         "Nb Jours": {
                             "formula": {"string": "Dans 2 jours"},
                         },
@@ -141,12 +74,6 @@ def test_agenda_results():
                 },
                 {
                     "properties": {
-                        "Date": {
-                            "date": {
-                                "start": "2022-12-11",
-                                "end": "2022-12-12",
-                            },
-                        },
                         "Nb Jours": {
                             "formula": {"string": "Dans 3 jours"},
                         },
@@ -161,12 +88,6 @@ def test_agenda_results():
                 },
                 {
                     "properties": {
-                        "Date": {
-                            "date": {
-                                "start": "2022-12-12T13:00:00.000+01:00",
-                                "end": "2022-12-14T12:00:00.000+01:00",
-                            },
-                        },
                         "Nb Jours": {
                             "formula": {"string": "Dans 5 jours"},
                         },
@@ -182,10 +103,10 @@ def test_agenda_results():
             ],
         }
     ) == [
-        ("08/12", "ajd", "test 1"),
-        ("09/12 13:12", "2j", "test 2"),
-        ("11/12-12/12", "3j", "test 3"),
-        ("12/12 13:00-14/12 12:00", "5j", "test 4"),
+        ("ajd", "test 1"),
+        ("2 j", "test 2"),
+        ("3 j", "test 3"),
+        ("5 j", "test 4"),
     ]
 
 
